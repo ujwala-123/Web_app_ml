b2eada8f73e9f9e6d24439797c061925a28c40f1
Ujwala Namineni <72090397+ujwala-123@users.noreply.github.com>
2023-06-21 16:39:21 -0400

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import index, predict
-urlpatterns = [
-    path('index/', index, name='index'),
-    path('', predict, name='predict'),
-]
